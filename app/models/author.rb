09dcc3ef1465ad5e1c328798429b6d20d8f940a1
Teej <gamesfreak26@gmail.com>
2021-08-23 15:15:56 +1000

    Add validations to author and book

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
     has_many :author_books, dependent: :destroy
     has_many :books, through: :author_books
+
+    validates :name, presence: true
+    validates :biography, length: {maximum: 250, too_long: "Max. 250 characters allowed"}
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
   has_many :author_books, dependent: :destroy
   has_many :authors, through: :author_books
   accepts_nested_attributes_for :author_books
+
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates_presence_of :description # min of 1
+  validates :description, length: {maximum: 250, too_long: "must have at most %{count} words"}
 end

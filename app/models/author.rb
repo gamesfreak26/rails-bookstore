18f850e1980d901a5e8fb9c61fb62e4b5c0e38f0
gamesfreak26 <gamesfreak26@gmail.com>
2021-08-24 11:13:44 +1000

    Fix author tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,6 @@ class Author < ApplicationRecord
     has_many :books, through: :author_books
 
     validates :name, presence: true
-    validates :biography, length: {maximum: 250, too_long: "Max. 250 characters allowed"}
+    validates :biography, length: {maximum: 2000, too_long: "must have at most %{count} words"}
 
 end
--- spec/factories/author.rb
+++ spec/factories/author.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
     factory :author do
       name { "Namenamename" }
       biography { "AAAA" }
-      dob { Date.parse('1960-11-10') }
-      date_of_birth { 18.years.ago }
+      dob { 18.years.ago }
     end
-  end   
+  end

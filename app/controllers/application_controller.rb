159abe71825dd29e294d4ff4c02d52a86a1e2dca
gamesfreak26 <gamesfreak26@gmail.com>
2021-08-25 13:32:59 +1000

    Add admin access to project.
    No idea if it's best practice.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    
+    protected
+    
+    def authenticate_admin!
+        authenticate_user!
+        redirect_to :books, status: :forbidden unless current_user.admin?
+    end
+      
 end

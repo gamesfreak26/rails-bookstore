e9ce81563d55ecc9bedb47b48723d78a70f82869
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2021-09-03 15:09:19 +1000

    Fix stripe not working on update and remove items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -12,6 +12,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update(order_params)
     @order_items = current_order.order_items
+    redirect_to cart_path
   end
 
   def destroy
